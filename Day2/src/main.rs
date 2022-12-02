d226fcc96b79cf09d4dc1cc5f00383d3a37a053a
Davide Rusconi <daviderusconi1@gmail.com>
2022-12-02 19:22:48 +0100

    Day2 - Prob2

--- Day2/src/main.rs
+++ Day2/src/main.rs
@@ -1,6 +1,7 @@
 use core::panic;
 use std::{env, fs};
 
+#[derive(Debug)]
 enum Output {
     Win,
     Loss,
@@ -8,6 +9,15 @@ enum Output {
 }
 
 impl Output {
+    fn parse(outcome : &str) -> Output{
+        match outcome {
+            "Z" => Output::Win,
+            "Y" => Output::Draw,
+            "X" => Output::Loss,
+            _ => panic!("Invalid outcome: {}",outcome)        
+        }
+    }
+
     fn evaluate(&self) -> i32{
         match self {
             Output::Win => 6,
@@ -18,6 +28,7 @@ impl Output {
 }
 
 #[derive(PartialEq)]
+#[derive(Debug)]
 enum Hand {
     Rock,
     Paper,
@@ -25,20 +36,11 @@ enum Hand {
 }
 
 impl Hand {
-    fn parse_elf(hand: &str) -> Hand{
+    fn parse(hand: &str) -> Hand{
         match hand {
             "A" => Hand::Rock,
             "B" => Hand::Paper,
             "C" => Hand::Scissor,
-            _ => panic!("Invalid elf hand: {}", hand)
-        }
-    }
-
-    fn parse_player(hand: &str) -> Hand{
-        match hand {
-            "X" => Hand::Rock,
-            "Y" => Hand::Paper,
-            "Z" => Hand::Scissor,
             _ => panic!("Invalid elf hand: {}", hand)
         }
     }
@@ -51,21 +53,55 @@ impl Hand {
         }
     }
 
+    fn fix_player(elf: Hand, outcome: Output) -> Hand {
+        match outcome {
+            Output::Win => match elf {
+                Hand::Rock => Hand::Paper,
+                Hand::Paper => Hand::Scissor,
+                Hand::Scissor => Hand::Rock
+            },
+            Output::Draw => elf,
+            Output::Loss => match elf {
+                Hand::Rock => Hand::Scissor,
+                Hand::Paper => Hand::Rock,
+                Hand::Scissor => Hand::Paper
+            }
+        }
+    }
+
 }
 
-fn evaulate_round(hands: &str, score: &mut i32){
-    let (elf, player) = (hands.split(" ").collect::<Vec<&str>>()[0], hands.split(" ").collect::<Vec<&str>>()[1]);
+fn convert_to_elf_hand(hand: &str) -> &str{
+    match hand {
+        "X" => "A",
+        "Y" => "B",
+        "Z" => "C",
+        _ => panic!("Invalid LHS, cannot convert {} to elf lingo",hand)
+    }
+}
+fn evaulate_round_p1(hand: &str, score: &mut i32){
+    let (elf, player) = (hand.split(" ").collect::<Vec<&str>>()[0], hand.split(" ").collect::<Vec<&str>>()[1]);
 
-    let mut outcome_score = match (Hand::parse_elf(elf), Hand::parse_player(player)) {
+    let mut outcome_score = match (Hand::parse(elf), Hand::parse(convert_to_elf_hand(player))) {
         (Hand::Rock,Hand::Paper) | (Hand::Paper,Hand::Scissor) | (Hand::Scissor, Hand::Rock) => {Output::Win},
         (x,y) => if x == y { Output::Draw} else { Output::Loss}
     }.evaluate();
 
-    outcome_score += Hand::parse_player(player).evaluate();
+    outcome_score += Hand::parse(convert_to_elf_hand(player)).evaluate();
 
     *score += outcome_score;
 
     println!("Hand ({},{}) lead to a score of {}", elf, player, outcome_score);
+}
+
+fn evaulate_round_p2(hand: &str, score: &mut i32) -> () {
+    let (elf, outcome) = (hand.split(" ").collect::<Vec<&str>>()[0], hand.split(" ").collect::<Vec<&str>>()[1]);
+    let player = Hand::fix_player(Hand::parse(elf), Output::parse(outcome));
+    
+    let hand_score = player.evaluate() + Output::parse(outcome).evaluate();
+    *score += hand_score;
+
+    println!("Output of hand ({},{}) is {} (player set to {:?} and outcome to {:?})", elf, outcome, hand_score,player,Output::parse(outcome));
 }
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -74,11 +110,14 @@ fn main() {
     }
 
     let input = fs::read_to_string(&args[1]).expect("Cannot read input file");
-    let mut total = 0;
+    let mut total_p1 = 0;
+    let mut total_p2 = 0;
 
     for entry in input.trim().split("\n") {
-        evaulate_round(entry, &mut total);
+        evaulate_round_p1(entry, &mut total_p1);
+        evaulate_round_p2(entry, &mut total_p2);
     }
 
-    println!("{}",total);
+    println!("Total as problem one rules: {}",total_p1);
+    println!("Total as problem two rules: {}",total_p2);
 }
