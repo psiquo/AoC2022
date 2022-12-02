1f478415986582f46c917f1a074ac33dae2e5d8f
Davide Rusconi <daviderusconi1@gmail.com>
2022-12-02 18:54:27 +0100

    Day1 - Prob2

--- Day1/src/main.rs
+++ Day1/src/main.rs
@@ -1,12 +1,18 @@
 use std::fs;
+use std::env;
 fn main() {
-    let input = fs::read_to_string("./input/input1.txt").expect("Cannot read input file");
-    let mut max : i32 = 0;
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 { 
+        panic!("Must provide a filename as argument");
+    }
+
+    let input = fs::read_to_string(&args[1]).expect("Cannot read input file");
+    let mut elf_calories : Vec<i32> = Vec::new();
     let mut tot : i32 = 0;
 
     for s in input.trim().split("\n") {
         if s == "" {
-            max = max.max(tot);
+            elf_calories.push(tot);
             tot = 0;
             continue;
         }
@@ -14,5 +20,7 @@ fn main() {
         tot += s.parse::<i32>().unwrap();
     }
 
-    println!("{}",max);
-}
+    elf_calories.sort_by(|a,b| b.cmp(a));
+    println!("Max: {}",elf_calories.iter().max().expect("Cannot find max"));
+    println!("Sum of the top three: {}", &elf_calories[0..3].iter().sum::<i32>());
+}   
