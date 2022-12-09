211d9891bb4f4a53c6bb2fb2292431af7611ddc5
Davide Rusconi <daviderusconi1@gmail.com>
2022-12-09 23:07:14 +0100

    Day8 - Prob 2

--- Day8/src/main.rs
+++ Day8/src/main.rs
@@ -14,6 +14,38 @@ fn get_column(matrix : &Vec<Vec<u32>>, col_index : usize) -> Vec<u32> {
 
     tmp
 }
+
+fn get_scenic_score(matrix : &Vec<Vec<u32>>, row : usize, column : usize) -> i32 {
+    let (r1, r2) = get_view_distance(&matrix[row], column);
+    let (c1,c2) = get_view_distance(&get_column(matrix, column),row);
+
+    r1 * r2 * c1 * c2
+}
+
+fn get_view_distance(row: &Vec<u32>, index: usize) -> (i32,i32) {
+    let mut c1 = 0;
+
+    for &el in row[..index].iter().rev() {
+        c1 += 1;
+
+        if el >= *row.get(index).unwrap() {
+            break;
+        }
+    }
+
+    let mut c2 = 0;
+
+    for &el in row[index+1..].iter() {
+        c2 += 1;
+
+        if el >= *row.get(index).unwrap() {
+            break;
+        }
+    }
+
+    (c1,c2)
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 { 
@@ -44,17 +76,19 @@ fn main() {
     println!("Height: {} Width: {}",height,width);
 
     let mut count = height * 2 + (width - 2) * 2;
+    let mut max_scenic = 0;
+
     for i in 1..(height-1) {
         for j in 1..(width-1){
             if !check_line(matrix.get(i).unwrap(), j) || 
                !check_line(&get_column(&matrix,j), i) {
-                //println!("{} is visible", matrix[i][j]);
                 count += 1;
-            } else {
-                //println!("{} is not visible",matrix[i][j]);
             }
+
+            max_scenic = max_scenic.max(get_scenic_score(&matrix, i, j));
         }
     }
 
-    println!("{}",count);
+    println!("Visibility count: {}",count);
+    println!("Max scenic score: {}",max_scenic);
 }
